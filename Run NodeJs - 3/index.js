59075b7926f60cf23101706e6a68787c419ddb04
abekolin <albertog4taz28@gmail.com>
2025-02-16 00:55:27 +0700

    menambahkan file

---
+++ Run NodeJs - 3/index.js
@@ -0,0 +1,6 @@
+// prompt terminal untuk run file index -> node .
+function hello(text){
+    return text;
+}
+
+module.exports = hello;
---
+++ Run NodeJs - 3/script.js
@@ -0,0 +1,4 @@
+const hello = require('./index');
+
+console.log(hello('Hello World!'))
+// console.log('oke');
